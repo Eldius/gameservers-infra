22126a2179481a7ccadbdb5edfce50e1da8ff2e3
Eldio Santos <eldio.santos@m4u.com.br>
2019-10-25 09:54:55 -0300

    Started to code the infra for Google Cloud Platform (GCP)

--- games/minecraft-java/gcp/main.tf
+++ games/minecraft-java/gcp/main.tf
@@ -8,6 +8,16 @@ terraform {
 }
 
 module "minecraft" {
-  source        = "../../../modules/minecraft-gcp"
+  # Game configs
+  source        = "../../../modules/google"
   servers_count = 1
+  playbook = "minecraft-java.yml"
+  instance_size = "s-1vcpu-2gb"
+  instance_name_pattern = "minecraft-test-server"
+  game_name = "minecraft-java-edition"
+
+  # Provider specific configs
+  disk_size = "10"
+  region = ""
+  machine_type = ""
 }
--- modules/google/variables.tf
+++ modules/google/variables.tf
@@ -1,12 +1,6 @@
 
-variable "servers_count" {
-  description = "Server count"
-  default = 1
-}
-
 variable "region" {}
 
-variable "amount" {}
 variable "name_prefix" {}
 variable "machine_type" {}
 variable "user_data" {}
@@ -32,4 +26,29 @@ variable "disk_destroy_local_exec_command_or_fail" {
 
 variable "disk_destroy_local_exec_command_and_continue" {
   default = ":"
+}
+
+variable "servers_count" {
+  description = "How much servers to create"
+  default     = 1
+}
+
+variable "playbook" {
+  description = "Playbook to execute"
+  default     = ""
+}
+
+variable "instance_size" {
+  description = "Server Size"
+  default     = ""
+}
+
+variable "instance_name_pattern" {
+  description = "Instance name pattern"
+  default     = ""
+}
+
+variable "game_name" {
+  description = "Game name"
+  default     = ""
 }
